6b0facad3797a6705db16c06570f8c314922a852
Lu Fengqi <lufq.fnst@cn.fujitsu.com>
2018-01-10 14:49:22 +0800

    btrfs-progs: add the stack prefix for backup_dev_root set/get function
    The backup_dev_root set/get function defined by
    BTRFS_SETGET_STACK_FUNCS macro is missing the prefix stack.
    
    Signed-off-by: Lu Fengqi <lufq.fnst@cn.fujitsu.com>

--- cmds-inspect-dump-tree.c
+++ cmds-inspect-dump-tree.c
@@ -100,7 +100,7 @@ static void print_old_roots(struct btrfs_super_block *super)
 
 		printf("\t\tdevice root gen %llu block %llu\n",
 		       (unsigned long long)btrfs_backup_dev_root_gen(backup),
-		       (unsigned long long)btrfs_backup_dev_root(backup));
+		       (unsigned long long)btrfs_stack_backup_dev_root(backup));
 
 		printf("\t\tcsum root gen %llu block %llu\n",
 		       (unsigned long long)btrfs_backup_csum_root_gen(backup),
